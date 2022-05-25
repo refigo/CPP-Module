dab5decb8115a45b7466d05be1d6548fc3c4bf1c
refigo <wlguq100@naver.com>
2022-05-25 13:09:14 +0900

    refactor: Module00/ex00

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -6,22 +6,19 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 21:09:23 by mgo               #+#    #+#             */
-/*   Updated: 2022/05/24 21:29:29 by mgo              ###   ########.fr       */
+/*   Updated: 2022/05/25 13:06:48 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-static char *set_to_upper(char *str)
+static char *set_to_uppers(char *str)
 {
 	int i;
 
 	i = -1;
 	while (str[++i])
-	{
-		if (('a' <= str[i]) && (str[i] <= 'z'))
-			str[i] -= 32;
-	}
+		str[i] = static_cast<char>(std::toupper(static_cast<unsigned char>(str[i])));
 	return (str);
 }
 
@@ -31,15 +28,13 @@ static void use_megaphone(int argc, char **argv)
 
 	i = 0;
 	while (++i < argc)
-	{
-		std::cout << set_to_upper(argv[i]);
-	}
+		std::cout << set_to_uppers(argv[i]);
 	std::cout << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-	if (argc == 1)
+	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 		use_megaphone(argc, argv);
