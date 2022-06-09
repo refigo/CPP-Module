a12205ebdfa1c2835276191a79291f9c3add0282
refigo <wlguq100@naver.com>
2022-06-09 09:54:58 +0900

    refactor: Account.cpp, ex02, Module00

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/09 09:20:39 by mgo               #+#    #+#             */
+/*   Updated: 2022/06/09 09:30:23 by mgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Account.hpp"
 #include <ctime>
 #include <iostream>
@@ -56,14 +68,16 @@ void Account::displayAccountsInfos(void)
 	[19920104_091532] index:7;amount:16576;created
 */
 Account::Account(int initial_deposit)
- : _accountIndex((Account::_nbAccounts)++), _amount(initial_deposit),
-	_nbDeposits(0), _nbWithdrawals(0)
 {	
+	_accountIndex = (Account::_nbAccounts)++;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	Account::_totalAmount += _amount;
 	Account::_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";created\n";
-	Account::_totalAmount += _amount;
 }
 
 /*
@@ -78,13 +92,12 @@ Account::Account(int initial_deposit)
 */
 Account::~Account(void)
 {
+	--(Account::_nbAccounts);
+	Account::_totalAmount -= _amount;
 	Account::_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";closed\n";
-
-	--(Account::_nbAccounts);
-	Account::_totalAmount -= _amount;
 }
 
 /*
@@ -102,17 +115,14 @@ void Account::makeDeposit(int deposit)
 	Account::_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";p_amount:" << _amount;
-
 	_amount += deposit;
 	++(_nbDeposits);
-
 	std::cout << ";deposit:" << deposit;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";nb_deposits:" << _nbDeposits;
 	std::cout << '\n';
-
 	Account::_totalAmount += deposit;
-	Account::_totalNbDeposits += 1;
+	++(Account::_totalNbDeposits);
 }
 
 /*
@@ -130,23 +140,20 @@ bool Account::makeWithdrawal(int withdrawal)
 	Account::_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";p_amount:" << _amount;
-
-	// todo: refactor
+	std::cout << ";withdrawal:";
 	if (withdrawal > _amount)
 	{
-		std::cout << ";withdrawal:" << "refused\n";
+		std::cout << "refused\n";
 		return (false);
 	}
 	_amount -= withdrawal;
 	++(_nbWithdrawals);
-
 	std::cout << ";withdrawal:" << withdrawal;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";nb_withdrawals:" << _nbWithdrawals;
 	std::cout << '\n';
-
 	Account::_totalAmount -= withdrawal;
-	Account::_totalNbWithdrawals += 1;
+	++(Account::_totalNbWithdrawals);
 	return (true);
 }
 
@@ -207,7 +214,7 @@ void Account::_displayTimestamp(void)
 	std::cout << std::setfill('0');
 	std::cout << '[';
 	std::cout << (1900 + timeinfo->tm_year);
-	std::cout << std::setw(2) << (timeinfo->tm_mon);
+	std::cout << std::setw(2) << (timeinfo->tm_mon + 1);
 	std::cout << std::setw(2) << (timeinfo->tm_mday);
 	std::cout << '_';
 	std::cout << std::setw(2) << (timeinfo->tm_hour);
