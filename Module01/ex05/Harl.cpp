1840037bf36e094e260b241ae37c6007022138e5
refigo <wlguq100@naver.com>
2022-06-20 14:48:56 +0900

    refactor: modify output text format for newline :::: ex05 Module01

--- Module01/ex05/Harl.cpp
+++ Module01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:05:16 by mgo               #+#    #+#             */
-/*   Updated: 2022/06/20 14:37:43 by mgo              ###   ########.fr       */
+/*   Updated: 2022/06/20 14:45:57 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,22 @@ void	Harl::debug(void)
 {
 	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love having extra bacon for my " \
-		"7XL-double-cheese-triple-pickle-specialketchup burger. " \
+		"7XL-double-cheese-triple-pickle-special-ketchup burger.\n" \
 		"I really do!\n";
 }
 
 void	Harl::info(void)
 {
 	std::cout << "[ INFO ]\n";
-	std::cout << "I cannot believe adding extra bacon costs more money. " \
-		"You didn’t put enough bacon in my burger! " \
+	std::cout << "I cannot believe adding extra bacon costs more money.\n" \
+		"You didn’t put enough bacon in my burger!\n" \
 		"If you did, I wouldn’t be asking for more!\n";
 }
 
 void	Harl::warning(void)
 {
 	std::cout << "[ WARNING ]\n";
-	std::cout << "I think I deserve to have some extra bacon for free. " \
+	std::cout << "I think I deserve to have some extra bacon for free.\n" \
 		"I’ve been coming for years " \
 		"whereas you started working here since last month.\n";
 }
@@ -53,7 +53,7 @@ void	Harl::warning(void)
 void	Harl::error(void)
 {
 	std::cout << "[ ERROR ]\n";
-	std::cout << "This is unacceptable! " \
+	std::cout << "This is unacceptable!\n" \
 		"I want to speak to the manager now.\n";
 }
 
