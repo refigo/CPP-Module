fef7d765f0da24f115c57edfb5a728363ebcca43
Refi Go <64242999+refigo@users.noreply.github.com>
2022-07-11 16:43:30 +0900

    feat: implement MateriaSource class :::: ex03 Module04

--- Module04/ex03/MateriaSource.cpp
+++ Module04/ex03/MateriaSource.cpp
@@ -29,18 +29,45 @@ MateriaSource::MateriaSource(const MateriaSource& orig) {
 }
 
 MateriaSource& MateriaSource::operator=(const MateriaSource& rhs) {
-
+  if (this != &rhs) {
+    for (int i = 0; i < MAX_MEM_MTRL_SLOTS; ++i) {
+      if (mem_mtrl_[i]) {
+        delete mem_mtrl_[i];
+        mem_mtrl_[i] = NULL;
+      }
+      if (rhs.mem_mtrl_[i])
+        mem_mtrl_[i] = rhs.mem_mtrl_[i]->clone();
+    }
+  }
+  return *this;
 }
 
 MateriaSource::~MateriaSource(void) {
-
+  for (int i = 0; i < MAX_MEM_MTRL_SLOTS; ++i) {
+    if (mem_mtrl_[i]) {
+      delete mem_mtrl_[i];
+      mem_mtrl_[i] = NULL;
+    }
+  }
 }
 
-void MateriaSource::learnMateria(AMateria* mtr) {
-
+void MateriaSource::learnMateria(AMateria* m) {
+  if (m == NULL)
+    return ;
+  for (int i = 0; i < MAX_MEM_MTRL_SLOTS; ++i) {
+    if (mem_mtrl_[i] == NULL) {
+      mem_mtrl_[i] = m;
+      return ;
+    }
+  }
 }
 
 AMateria* MateriaSource::createMateria(const std::string& type) {
-
+  for (int i = 0; i < MAX_MEM_MTRL_SLOTS; ++i) {
+    if (mem_mtrl_[i] && (mem_mtrl_[i]->getType() == type)) {
+      return (mem_mtrl_[i]->clone());
+    }
+  }
+  return NULL;
 }
 
