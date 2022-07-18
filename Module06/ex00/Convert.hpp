5a00abb7c64e0903b528958393f7099bfe649d0f
refigo <wlguq100@naver.com>
2022-07-18 19:14:43 +0900

    feat: add initila Convert class :::: ex00 Module06

--- Module06/ex00/Convert.hpp
+++ Module06/ex00/Convert.hpp
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Convert.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/18 09:18:02 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/18 09:36:54 by mgo              ###   ########.fr       */
+/*   Created: 2022/07/18 09:39:46 by mgo               #+#    #+#             */
+/*   Updated: 2022/07/18 09:41:31 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONVERT_HPP
+# define CONVERT_HPP
+
 #include <iostream>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cout << "./convert [ string to convert ]\n";
-    return 1;
-  }
-  // tood: convert to char
-  // tood: convert to int
-  // tood: convert to float
-  // tood: convert to double
-  
-  return 0;
-}
+class Convert {
+ public:
+  Convert(const Convert& orig);
+  Convert& operator=(const Convert& rhs);
+  ~Convert(void);
+
+ private:
+  Convert(void);
+};
+
+#endif
