71f7b828fe2c7ccb9eb0ed1b53444dc10d74f957
refigo <wlguq100@naver.com>
2022-07-25 09:48:04 +0900

    feat: implement template function swap(), min() and max() and set test case :::: ex00 Module07

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -6,12 +6,55 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 12:20:06 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/21 12:20:36 by mgo              ###   ########.fr       */
+/*   Updated: 2022/07/25 09:47:14 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include <iostream>
+
+void testPlace(void) {
+  std::cout << std::endl;
+
+  // testing pdf
+  std::cout << "<Start pdf test>\n";
+  {
+    int a = 2;
+    int b = 3;
+  
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+  }
+  std::cout << "<Done pdf test>\n";
+
+  std::cout << std::endl;
+
+  // testing when equal
+  std::cout << "<Start equal test>\n";
+  {
+    int first(42), second(42);
+
+    std::cout << "address of first: [" << &first << "]\n";
+    std::cout << "address of second: [" << &second << "]\n";
+    std::cout << "min() return value: [" << min(first, second) << "]\n";
+    std::cout << "address of min() return value: [" << &min(first, second) << "]\n";
+    std::cout << "max() return value: [" << max(first, second) << "]\n";
+    std::cout << "address of max() return value: [" << &max(first, second) << "]\n";
+  }
+  std::cout << "<Start equal test>\n";
+
+  std::cout << std::endl;
+}
 
 int main(void) {
+  testPlace();
   return 0;
 }
--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -6,13 +6,28 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 12:19:46 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/21 12:20:04 by mgo              ###   ########.fr       */
+/*   Updated: 2022/07/25 09:24:11 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+template <typename T>
+void swap(T& a, T& b) {
+  T temp(a);
+  a = b;
+  b = temp;
+}
 
+template <typename T>
+T& min(T& a, T& b) {
+  return (a < b ? a : b);
+}
+
+template <typename T>
+T& max(T& a, T& b) {
+  return (a > b ? a : b);
+}
 
 #endif
