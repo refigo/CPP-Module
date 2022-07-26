5dcf46fad3a09b9e5f22fcd33b7e9df265079185
refigo <wlguq100@naver.com>
2022-07-26 19:32:35 +0900

    refact: add text in testPlace() :::: ex02 Module08

--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 18:09:26 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/26 19:15:07 by mgo              ###   ########.fr       */
+/*   Updated: 2022/07/26 19:31:57 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 void testPlace(void) {
   std::cout << std::endl;
 
+  // pdf test
   {
     MutantStack<int> mstack;
     mstack.push(5);
@@ -44,30 +45,9 @@ void testPlace(void) {
 
   std::cout << std::endl;
 
-  // pdf test replaced std::list
-  {
-    std::list<int> mstack;
-    mstack.push_back(5);
-    mstack.push_back(17);
-    std::cout << mstack.back() << std::endl;
-    mstack.pop_back();
-    std::cout << mstack.size() << std::endl;
-    mstack.push_back(3);
-    mstack.push_back(5);
-    mstack.push_back(737);
-    //[...]
-    mstack.push_back(0);
-    std::list<int>::iterator it = mstack.begin();
-    std::list<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-      std::cout << *it << std::endl;
-      ++it;
-    }
-    //std::stack<int> s(mstack);
-  }
+  // test iterator
+  // test reverse_iterator
+  // test various types
 
   std::cout << std::endl;
 }
