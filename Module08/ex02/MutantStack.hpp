7ef49d44ce210a70bfd47bb0cbd55d92e45d9f09
refigo <wlguq100@naver.com>
2022-07-26 19:17:52 +0900

    feat: implement MutantStack class :::: ex02 Moduel08

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -6,11 +6,42 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 18:09:04 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/26 18:09:24 by mgo              ###   ########.fr       */
+/*   Updated: 2022/07/26 19:12:17 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANT_STACK_HPP
 #define MUTANT_STACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+ public:
+  typedef typename std::stack<T>::container_type::iterator 
+    iterator;
+  typedef typename std::stack<T>::container_type::const_iterator 
+    const_iterator;
+  typedef typename std::stack<T>::container_type::reverse_iterator 
+    reverse_iterator;
+  typedef typename std::stack<T>::container_type::const_reverse_iterator 
+    const_reverse_iterator;
+
+  iterator begin(void) {
+    return (this->c.begin());
+  }
+  iterator end(void) {
+    return (this->c.end());
+  }
+
+  reverse_iterator rbegin(void) {
+    return (this->c.rbegin());
+  }
+  reverse_iterator rend(void) {
+    return (this->c.rend());
+  }
+
+ private:
+};
 
 #endif
--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -6,12 +6,73 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 18:09:26 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/26 18:09:54 by mgo              ###   ########.fr       */
+/*   Updated: 2022/07/26 19:15:07 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <stack>
+#include <list>
+
+void testPlace(void) {
+  std::cout << std::endl;
+
+  {
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    std::stack<int> s(mstack);
+  }
+
+  std::cout << std::endl;
+
+  // pdf test replaced std::list
+  {
+    std::list<int> mstack;
+    mstack.push_back(5);
+    mstack.push_back(17);
+    std::cout << mstack.back() << std::endl;
+    mstack.pop_back();
+    std::cout << mstack.size() << std::endl;
+    mstack.push_back(3);
+    mstack.push_back(5);
+    mstack.push_back(737);
+    //[...]
+    mstack.push_back(0);
+    std::list<int>::iterator it = mstack.begin();
+    std::list<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    //std::stack<int> s(mstack);
+  }
+
+  std::cout << std::endl;
+}
 
 int main(void) {
+  testPlace();
   return 0;
 }
