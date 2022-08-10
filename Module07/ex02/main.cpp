c7432175f3728eb035d4e382cdf6673a31b62231
refigo <wlguq100@naver.com>
2022-08-10 13:01:20 +0900

    feat: add provided test case :::: ex02 Module07

--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 11:50:26 by mgo               #+#    #+#             */
-/*   Updated: 2022/07/27 18:26:41 by mgo              ###   ########.fr       */
+/*   Updated: 2022/08/10 13:00:13 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,3 +160,59 @@ int main(void) {
   system("leaks a.out");
   return 0;
 }
+
+/*
+#include <iostream>
+#include <Array.hpp>
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+  Array<int> numbers(MAX_VAL);
+  int* mirror = new int[MAX_VAL];
+  srand(time(NULL));
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    const int value = rand();
+    numbers[i] = value;
+    mirror[i] = value;
+  }
+  //SCOPE
+  {
+    Array<int> tmp = numbers;
+    Array<int> test(tmp);
+  }
+
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    if (mirror[i] != numbers[i])
+    {
+      std::cerr << "didn't save the same value!!" << std::endl;
+      return 1;
+    }
+  }
+  try
+  {
+    numbers[-2] = 0;
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  try
+  {
+    numbers[MAX_VAL] = 0;
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+
+  for (int i = 0; i < MAX_VAL; i++)
+  {
+    numbers[i] = rand();
+  }
+  delete [] mirror;//
+  return 0;
+}
+*/
